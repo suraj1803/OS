13387fd7170e8e112c413c39b218f5aafabd8228
Suraj Biswas <surajbiswas182003@gmail.com>
2025-01-14 16:35:45 +0530

    fix: modified the exisiting code

--- shell.c
+++ shell.c
@@ -2,10 +2,12 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <stdlib.h>
+#include <stdlib.h> 
 #define MAX_LINE 80
 
-void separate_tokens(char* input, char* dest[], int size);
+int AND_ENCOUNTERED = 0;
+
+int separate_tokens(char* input, char* dest[], int size);
 void free_args(char* args[]);
 
 int main() {
@@ -13,19 +15,21 @@ int main() {
   char* args[arg_len];
   int should_run = 1;
 
+
   while (should_run) {
-    printf("suraj $ ");
+    printf("suraj-> ");
     fflush(stdout);
 
     char input[MAX_LINE];
     fgets(input, MAX_LINE, stdin);
 
-    if (strcmp(input, "exit\n") == 0) {
+    int tokens = separate_tokens(input, args, arg_len);
+
+    if (strcmp(args[0], "exit") == 0) {
       should_run = 0;
+      free_args(args);
       continue;
     }
-
-    separate_tokens(input, args, arg_len);
 
     if (fork() == 0) {
       execvp(args[0], args);
@@ -40,49 +44,60 @@ int main() {
   return 0;
 }
 
-void separate_tokens(char* input, char* dest[], int size) {
+int separate_tokens(char* input, char* dest[], int size) {
   int token = 0;
   char* str = malloc(30);
   int idx = 0;
   int i = 0;
   int IN = 0;
-  int OUT = 1;
   while (token < size - 1) {
     if (input[i] == ' ' || input[i] == '\n') {
-      if (IN && !OUT) {
+      if (IN) {
         str[idx] = '\0';
         dest[token] = malloc(strlen(str) + 1);
         strcpy(dest[token], str);
         idx = 0;
         token++;
         IN = 0;
-        OUT = 1;
         if (input[i] == '\n') {
           break;
         }
       }
-        if (input[i] == '\n') {
-          break;
-        }
+      if (input[i] == '\n') {
+        break;
+      }
     }
     else {
-      OUT = 0;
+      if (input[i] == '&') {
+        AND_ENCOUNTERED = 1;
+        i++;
+        continue;
+      }
       IN = 1;
       str[idx] = input[i];
       idx++;
     }
-
     i++;
   }
 
   dest[token] = NULL;
-
   free(str);
+  return token + 1;
 }
 
 void free_args(char* args[]) {
-    int i = 0;
-    while (args[i]) {
-      free(args[i++]);
+  int i = 0;
+  while (args[i] != NULL) {
+    free(args[i]);
+    i++;
+  }
+}
+
+void remove_spaces(char* s) {
+  char* d = s;
+  do {
+    while (*d == ' ') {
+      ++d;
     }
+  } while ((*s++ = *d++));
 }
--- tests/test.c
+++ tests/test.c
@@ -3,12 +3,14 @@
 #include <string.h>
 
 void separate_tokens(char* input, char* dest[], int size) {
+
   int token = 0;
   char* str = malloc(30);
   int idx = 0;
   int i = 0;
   int IN = 0;
   int OUT = 1;
+
   while (token < size - 1) {
     if (input[i] == ' ' || input[i] == '\n') {
       if (IN && !OUT) {
@@ -42,18 +44,41 @@ void separate_tokens(char* input, char* dest[], int size) {
   free(str);
 }
 
+void remove_spaces(char* s) {
+  char* d = s;
+  int IN = 0;
+  int OUT = 1;
+  do {
+    if (*d != ' ') {
+      IN = 1;
+      OUT = 0;
+    }
+    while (*d == ' ') {
+      if (!IN && OUT) {
+        d++;
+      }
+      else {
+        IN = 0;
+        OUT = 1;
+      }
+    }
+  } while ((*s++ = *d++));
+}
+
 int main() {
   char* dest[50];
   char* input = malloc(100);
   fgets(input, 100, stdin);
   /*printf("%d\n", input[strlen(input)]);*/
 
-  separate_tokens(input, dest, 50);
+  remove_spaces(input);
+  /*separate_tokens(input, dest, 50);*/
+  printf("%s", input);
 
-  for (int i = 0; dest[i] != NULL; i++) {
-    printf("%s\n", dest[i]);
-    free(dest[i]);
-  }
+  /*for (int i = 0; dest[i] != NULL; i++) {*/
+  /*  printf("%s\n", dest[i]);*/
+  /*  free(dest[i]);*/
+  /*}*/
 
   free(input);
 
